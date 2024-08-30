2ede1665a16bbb5cbf1eca8f6d7f778f36ca7f20
LilyBurton <lily.burton1992@gmail.com>
2024-08-30 16:55:02 +0100

    first commit

---
+++ build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let user = 'Lily';
+console.log(user);
+let a = 20;
+let b = "13";
+let c = 2;
+console.log(a * b);
+console.log(c + b);
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+let user = 'Lily'
+console.log(user)
+
+let a: number = 20
+let b: string = "13"
+let c: number = 2
+
+console.log(a * b)
+
+console.log(c + b)

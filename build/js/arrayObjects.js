2599adf56d6bc65dfbb76f71de4d4f5911c1d346
LilyBurton <lily.burton1992@gmail.com>
2024-09-10 17:37:48 +0100

    arrays and objects

---
+++ build/js/arrayObjects.js
@@ -0,0 +1,25 @@
+"use strict";
+let stringArr = ['Erika', 'Blake', 'Amir'];
+let stringAndNumbers = ['one', 2, '3'];
+let mixedData = ['mixed', 4, true];
+stringArr[0] = 'Joel';
+stringArr.push('Krystina');
+stringAndNumbers[0] = 1992;
+stringAndNumbers.unshift('Four');
+let test = [];
+let books = [];
+books.push('To Gaze Upon Wicked Gods');
+// tuple
+let myTuple = ['Lily', 42, false];
+let mixed = ['Jess', 12, true];
+myTuple[1] = 13;
+// Objects
+let myObj;
+myObj = [];
+console.log(typeof myObj);
+myObj = books;
+myObj = {};
+const exampleObj = {
+    prop1: 'Blake',
+    prop2: true
+};

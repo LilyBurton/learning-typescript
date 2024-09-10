c90bf05748ca36d0ffe5c25b7907ab5f357a95a7
LilyBurton <lily.burton1992@gmail.com>
2024-09-10 15:19:23 +0100

    Understanding basic types

---
+++ src/basicTypes.ts
@@ -0,0 +1,18 @@
+let firstName: string = 'Lily'
+let meaningOfLife: number;
+let isLoading: boolean;
+let book: any;
+
+firstName = 'Jean'
+meaningOfLife = 42
+isLoading = true
+book = 'The Poppy War'
+
+const sum = (a: number, b: number) => {
+    return a + b
+}
+
+let postId: string | number
+let isActive: boolean | number
+
+let re: RegExp = /\w+/g

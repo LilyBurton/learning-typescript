f5f556e781495da79f1771e33f3b5d62a38b555f
LilyBurton <lily.burton1992@gmail.com>
2024-09-23 15:38:21 +0100

    learning about arrays, objects and enums

--- build/js/arrayObjects.js
+++ build/js/arrayObjects.js
@@ -23,3 +23,32 @@ const exampleObj = {
     prop1: 'Blake',
     prop2: true
 };
+const actor1 = {
+    name: 'Keith David',
+    active: true,
+    filmography: ['The Princess and The Frog', 'Rick and Morty', 'Hazbin Hotel', 'Coraline']
+};
+const actor2 = {
+    name: 'Marilyn Monroe',
+    active: false,
+    filmography: ['Some Like it Hot', 'Gentlemen Prefers Blondes', 'How to Marry a Millionaire', 'The Seven Year Itch']
+};
+const greetActor = (actor) => {
+    var _a;
+    if (actor.name) {
+        return `Hello ${(_a = actor.name) === null || _a === void 0 ? void 0 : _a.toUpperCase()}!`;
+    }
+    return 'Hello!';
+};
+console.log(greetActor(actor2));
+// Enums
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 0] = "U";
+    Grade[Grade["D"] = 1] = "D";
+    Grade[Grade["C"] = 2] = "C";
+    Grade[Grade["B"] = 3] = "B";
+    Grade[Grade["A"] = 4] = "A";
+    Grade[Grade["S"] = 5] = "S";
+})(Grade || (Grade = {}));
+console.log(Grade.S);
--- src/arrayObjects.ts
+++ src/arrayObjects.ts
@@ -32,3 +32,44 @@ const exampleObj = {
     prop1: 'Blake',
     prop2: true
 }
+
+interface Actor {
+    name?: String,
+    active: boolean,
+    filmography: (string | number)[]
+}
+
+const actor1: Actor = {
+    name: 'Keith David', 
+    active: true,
+    filmography: ['The Princess and The Frog', 'Rick and Morty', 'Hazbin Hotel', 'Coraline']
+}
+
+const actor2: Actor = {
+    name: 'Marilyn Monroe',
+    active: false,
+    filmography: ['Some Like it Hot', 'Gentlemen Prefers Blondes', 'How to Marry a Millionaire', 'The Seven Year Itch']
+}
+
+const greetActor = (actor: Actor) => {
+    if (actor.name) {
+        return `Hello ${actor.name?.toUpperCase()}!`
+    } return 'Hello!'
+    
+}
+
+console.log(greetActor(actor2))
+
+// Enums
+
+enum Grade {
+    U,
+    D,
+    C,
+    B,
+    A,
+    S
+}
+
+console.log(Grade.S)
+

c4a2994c8d4a9603b8b0374d430d77b45416e9d8
LilyBurton <lily.burton1992@gmail.com>
2025-01-07 16:29:50 +0000

    Finished lesson on signatures

--- build/js/signatures.js
+++ build/js/signatures.js
@@ -17,3 +17,26 @@ const todaysNet = (transactions) => {
     return total;
 };
 console.log(todaysNet(todaysTransaction));
+const student = {
+    name: 'Penelope',
+    GPA: 5,
+    classes: [100, 200]
+};
+for (const key in student) {
+    console.log(`${key}: ${student[key]}`);
+}
+Object.keys(student).map(key => {
+    console.log(student[key]);
+});
+const logStudentKey = (student, key) => {
+    console.log(`Student ${key}: ${student[key]}`);
+};
+logStudentKey(student, 'name');
+const monthlyIncomes = {
+    salary: 500,
+    bonus: 100,
+    sidehustle: 250
+};
+for (const revenue in monthlyIncomes) {
+    console.log(monthlyIncomes[revenue]);
+}
--- src/signatures.ts
+++ src/signatures.ts
@@ -33,3 +33,51 @@ number => {
 }
 
 console.log(todaysNet(todaysTransaction))
+////////////////////////////////////////////////////////////
+
+interface Student {
+    // [key: string]: string | number | number[] | undefined
+    name: string,
+    GPA: number,
+    classes?: number[]
+}
+
+const student: Student = {
+    name: 'Penelope',
+    GPA: 5,
+    classes: [100, 200]
+}
+
+for (const key in student) {
+    console.log(`${key}: ${student[key as keyof Student]}`)
+}
+
+Object.keys(student).map(key => {
+    console.log(student[key as keyof typeof student])
+})
+
+const logStudentKey = (student: Student, key: keyof Student):
+void => {
+    console.log(`Student ${key}: ${student[key]}`)
+}
+
+logStudentKey(student, 'name')
+///////////////////////////////////////////////////////////////////
+
+//  interface Incomes {
+//     [key: string]: number
+//  }
+
+type Streams = 'salary' | 'bonus' | 'sidehustle'
+
+type Incomes = Record<Streams, number | string>
+
+const monthlyIncomes: Incomes = {
+    salary: 500,
+    bonus: 100,
+    sidehustle: 250
+}
+
+for (const revenue in monthlyIncomes) {
+    console.log(monthlyIncomes[revenue as keyof Incomes])
+}
